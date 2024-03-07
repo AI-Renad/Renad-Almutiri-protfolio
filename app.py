45a8a24f3014ca1a150ec3bcefc45d3e03abf3f2
AI-Renad <162414686+AI-Renad@users.noreply.github.com>
2024-03-07 11:32:54 +0000

    page with style
    page with style

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
-  return "Hello, Renad!"
-  
-if __name__=="__main__":
-  app.run(host="0.0.0.0",port=True)#run flack server
+  return render_template("home.html")
+
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=True)  #run flack server
