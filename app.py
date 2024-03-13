14aa9c8d9088aea5eaf7a7d919ab7998b788ba71
AI-Renad <162414686+AI-Renad@users.noreply.github.com>
2024-03-13 09:02:40 +0000

    use dictionary to add data in website

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask,render_template
 
 app = Flask(__name__)
-
+project=[
+  {'id':1,
+  'project':'web scrapping',
+  'skills':'urllib python,Python,Requests,Selenium'},
+  {'id':2,
+   'project':'AI model for flare mointoring',
+  'skills':'python padas, skilitlib , matblotlib , microsoft azur,pychatgbt'}
+]
 
 @app.route("/")
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html",myskills=project,
+                        myname='Renad')
 
 
 if __name__ == "__main__":
